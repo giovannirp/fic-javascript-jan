ddc9e98bd9bf7a5a1a0c06c2c826a881e4ed5873
giovannirp <giovanni_ge@hotmail.com>
2025-02-05 14:53:52 -0300

    add pdf e criando o primeiro JS separado na aula

---
+++ 05-02/script.js
@@ -0,0 +1,9 @@
+// Variável
+let nome = prompt("Digite seu nome");
+let idade = prompt("Digite sua idade");
+let time = prompt("Digite seu time");
+
+// Imprimir resposta
+document.write("Seja bem vindo: " + nome + ", " + " Sua idade é: " + idade + " Anos.");
+document.write("<br><br>");
+document.write(`Seja bem vindo: ${nome}, sua idade é: ${idade} anos`); 

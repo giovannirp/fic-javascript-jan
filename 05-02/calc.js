fe268e4fd8be6889cfe6a24299b2feef4eaa87b0
giovannirp <giovanni_ge@hotmail.com>
2025-02-06 14:05:07 -0300

    adicionando correção do exer1

--- 05-02/calc.js
+++ 05-02/calc.js
@@ -4,8 +4,8 @@
 let num1 = Number(prompt("Digite um número"));
 let num2 = Number(prompt("Digite outro número"));
 
-console.log(typeof num1);
-console.log(typeof num2);
+// Operações
+let soma = num1 + num2;
 
 // imprimir resposta
-console.log(`A soma é ${num1 + num2}`);
+console.log(`A soma é ${soma}`);

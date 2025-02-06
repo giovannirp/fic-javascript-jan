ef7ee96be2f74c2943f6dd93eee4a67ae7725111
giovannirp <giovanni_ge@hotmail.com>
2025-02-06 14:41:18 -0300

    Adicionando tipos de dados

---
+++ 06-02/tipos-dados.js
@@ -0,0 +1,16 @@
+// String
+let nome = "Giovanni";
+
+// Inteiro
+let idade = 37;
+
+// Booleano
+let casado = true;
+
+// Números flutuantes com casas decimais
+let valorProduto = 5.35;
+
+console.log("Meu nome é: ", nome);
+console.log("Idade", idade);
+console.log("Casado? " + casado);
+console.log("R$", valorProduto);
---
+++ 06-02/variavel.js
@@ -0,0 +1,12 @@
+let mensagem = "Olá, mundo";
+const nomeCompleto = "Giovanni Ribeiro";
+
+// nomeCompleto = "Caio Fernandes";
+
+// Idade
+let idade = 37;
+
+idade = 38;
+
+console.log(nomeCompleto);
+console.log(`${mensagem} e a idade é: ${idade}`);

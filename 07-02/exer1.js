dcc136f90f0e1503b265accbdb3a66ea8de15089
giovannirp <giovanni_ge@hotmail.com>
2025-02-07 15:26:01 -0300

    adicionando arquivos

---
+++ 07-02/exer1.js
@@ -0,0 +1,19 @@
+/*
+    Use o método adequado para adicionar os 3 seguintes itens
+
+    - Coxinha
+    - null
+    - Brigadeiro
+*/
+let brazilianFoods = [];
+
+// ["coxinha", null, "brigadeiro"];
+brazilianFoods.push("Coxinha", null, "Brigadeiro");
+//                     0        1       2
+
+// No lugar do null, adicionar Pão de Queijo
+console.log(brazilianFoods[1])
+
+brazilianFoods[1] = "Pão de Queijo";
+console.log(brazilianFoods);
+

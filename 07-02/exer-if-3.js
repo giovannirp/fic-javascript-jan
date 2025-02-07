afa8f0b6ce0cf3a2557e4275bc19696ec9bcc7ff
giovannirp <giovanni_ge@hotmail.com>
2025-02-07 16:20:28 -0300

    adicionando

---
+++ 07-02/exer-if-3.js
@@ -0,0 +1,6 @@
+/*
+    se idade >= 18, a pessoa pode ir para festa
+    senão, a pessoa não pode ir para festa
+*/
+let idade = 18;
+
---
+++ 07-02/exer2.js
@@ -0,0 +1,5 @@
+const maleDogNames = ["Chico", "Zeca"];
+const femaleDogNames = ["Doris", "Juliana"];
+
+const namesGeneral = maleDogNames.concat(femaleDogNames);
+console.log(namesGeneral)

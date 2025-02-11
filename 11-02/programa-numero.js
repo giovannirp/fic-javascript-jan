c6b7afa13b458641a0d00736ca86a76501194e11
giovannirp <giovanni_ge@hotmail.com>
2025-02-11 14:30:19 -0300

    feat: adicionando arquivos

---
+++ 11-02/programa-numero.js
@@ -0,0 +1,16 @@
+/*
+    - Verifica se o número é positivo, negativo ou zero
+    se número for maior que zero, é positivo
+    se o número for menor que zero, é negativo
+    senão o número não é válido
+*/
+let numero = prompt("Digite um número");
+
+// Verifica se o número é positivo, negativo ou zero
+if (numero > 0) {
+    document.write(`o número ${numero} é positivo`);
+} else if (numero < 0) {
+    document.write(`o número ${numero} é negativo `)
+} else {
+    document.write(`O número ${numero}, não é válido`)
+}

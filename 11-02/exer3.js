1f3fc65d9366a51eb409f98ff1925f5aafebc9be
giovannirp <giovanni_ge@hotmail.com>
2025-02-11 16:17:59 -0300

    adicionando exer3 e valor2 no projeto

---
+++ 11-02/exer3.js
@@ -0,0 +1,12 @@
+/*
+  - Armazene sua idade em uma constante;
+  - Escreva uma estrutura condicional em que, se a idade é 7 anos ou menos, ou  
+    se a idade é 65 anos ou mais, a frase "Para você, a entrada é grátis!" é  
+    armazenada em uma variável;
+  - Se a idade não atende às condições acima, a variável deve armazenar a  
+    mensagem "A entrada é R$ 30,00.";
+  - Exiba a mensagem no console;
+  - Agora, teste diferentes idades para se certificar que a condição do `if`  
+    também está funcionando.
+*/
+

2e99438e43a178904beb74ddb226d231afb5a328
giovannirp <giovanni_ge@hotmail.com>
2025-02-12 13:52:20 -0300

    feat: add

--- 11-02/exer3.js
+++ 11-02/exer3.js
@@ -9,4 +9,11 @@
   - Agora, teste diferentes idades para se certificar que a condição do `if`  
     também está funcionando.
 */
+let age = 64;
+
+if (age <=7 || age >= 65) {
+  console.log(`Para você, a entrada é grátis!`);
+} else {
+  console.log(`A entrada é R$30,00`);
+}
 

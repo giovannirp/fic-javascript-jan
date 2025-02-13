fe12a81b686caff65a7b458255bd7d1e5a9cd733
giovannirp <giovanni_ge@hotmail.com>
2025-02-13 14:29:12 -0300

    feat: adicionando exercicio novo

---
+++ 12-02/exer-for.js
@@ -0,0 +1,14 @@
+/*
+      - Após a const names, utilize um for loop para gerar um template HTML;
+  - Exiba no console o template HTML que você gerou;
+  - O template deve ser exibido apenas uma vez no console e o resultado deve ser:
+    <li>João Grilo</li><li>Chicó</li><li>Rosinha</li>
+*/
+const names = ['João Grilo', 'Chicó', 'Rosinha'];
+let templateNames = '';
+
+for (let i = 0; i < names.length; i++) {
+    templateNames += `<li>${names[i]}</li>`;
+}
+
+console.log(templateNames)
---
+++ 12-02/while-exer.js
@@ -0,0 +1,10 @@
+/*
+    tabuada 7
+*/
+let multiplicador = 1;
+
+while(multiplicador <= 10) {
+    
+    document.write(" 7 * " + multiplicador + " = " + 7 * multiplicador + "<br>");
+    multiplicador += 1;
+}

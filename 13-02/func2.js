c48ce531c48378d6b557dd6e931182dfd8b9f35d
giovannirp <giovanni_ge@hotmail.com>
2025-02-13 16:21:17 -0300

    feat: adicionando código

---
+++ 13-02/func2.js
@@ -0,0 +1,15 @@
+function mostrarNome(nome) {
+    document.write(nome);
+}
+// Função para criar frase
+// Mostrar Frase
+
+function pularLinha() {
+    document.write("<br><br>");
+}
+pularLinha();
+mostrarNome("Fabio");
+pularLinha();
+mostrarNome("Caio");
+pularLinha();
+mostrarNome("Carla");

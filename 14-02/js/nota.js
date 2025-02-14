9a9b2d379f9c7654869ec679a58ea4bc7c52c9cf
giovannirp <giovanni_ge@hotmail.com>
2025-02-14 15:48:22 -0300

    feat: adicionando js

---
+++ 14-02/js/nota.js
@@ -0,0 +1,8 @@
+// Variáveis
+const inputNota = document.getElementById("nota");
+const resultado = document.getElementById("resultado");
+
+function resultadoNotaClick() {
+    console.log("Olá Giovanni");
+    alert("Olá Giovanni");
+}

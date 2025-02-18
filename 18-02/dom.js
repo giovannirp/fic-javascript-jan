3751e6f3dfbcacbe26be74df18758ffbe45c084c
giovannirp <giovanni_ge@hotmail.com>
2025-02-18 16:16:57 -0300

    adicionando projeto

--- 18-02/dom.js
+++ 18-02/dom.js
@@ -5,13 +5,17 @@ const email = document.getElementById("email");
 
 const titulo = document.getElementById("titulo");
 const subtitulo = document.getElementById("subtitulo");
+const txtEmail = document.getElementById("txt-email");
 
 function mudar(event) {
     event.preventDefault();
-    console.log(nome.value);
 
     // Imprimir na tela
     titulo.innerHTML = nome.value;
     subtitulo.innerHTML = cidade.value;
+    txtEmail.innerHTML = email.value;
+
+    // Limpar os campos
+    nome.value = "";
 
 }

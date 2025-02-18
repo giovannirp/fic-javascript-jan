720ccb43b13b5ac07d31d685777753611ed23eee
giovannirp <giovanni_ge@hotmail.com>
2025-02-18 14:36:37 -0300

    adicionando validação e lógica da mensagem

--- 14-02/js/nota.js
+++ 14-02/js/nota.js
@@ -1,11 +1,20 @@
 // Variáveis
 const inputNota = document.getElementById("nota");
 const resultado = document.getElementById("resultado");
+const message = document.getElementById("message");
 
 function resultadoNotaClick(event) {
     // previne um comportamento no navegador
     event.preventDefault();
     console.log(inputNota.value);
+
+    // Validação de formulário
+    if (inputNota.value === "") {
+        message.style.display = "block";
+        return false;
+    }
+
+    message.style.display = "none";
 
     // condição
     if (inputNota.value >=6) {

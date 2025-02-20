2891bd59686eef3545af6c28bec6e9d90b759852
giovannirp <giovanni_ge@hotmail.com>
2025-02-20 14:41:52 -0300

    feat: adicionando

---
+++ 20-02/js/cadastro.js
@@ -0,0 +1,16 @@
+// Variáveis
+const campo = document.querySelector("#campo");
+const salvarBtn = document.querySelector("#salvar");
+
+// Adicionar evento de clique ao botão salvar
+salvarBtn.addEventListener("click", function() {
+    const valor = campo.value;
+    
+    // Salvar no localStorage
+    const chave = `ValorSalvo_${Date.now()}`; // usa timestamp para chave única
+    // Funcionalidade do LocalStorage
+    localStorage.setItem(chave, valor);
+
+    // Limpar campo após salvar
+    campo.value = "";
+});

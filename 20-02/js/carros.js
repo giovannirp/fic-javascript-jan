7222ebc0d1935dd7d58749dd7aead2f1a884a21c
giovannirp <giovanni_ge@hotmail.com>
2025-02-21 14:43:58 -0300

    Adicionando objetos carros

---
+++ 20-02/js/carros.js
@@ -0,0 +1,15 @@
+const cars = [
+    {
+        brand: "Toyota",
+        model: "Corola",
+        year: 2022
+    },
+    {
+        brand: "Honda",
+        model: "Civic",
+        year: 2008
+    }
+]
+
+console.log(cars);
+

fa4188c552e5d46506f04a6ae228ecad7c553784
Giovanni <giovanni_ge@hotmail.com>
2025-06-13 10:42:34 -0300

    feat: add

--- projeto-requisicao/js/conectaApi.js
+++ projeto-requisicao/js/conectaApi.js
@@ -23,11 +23,23 @@ async function criaVideo(titulo, descricao, url, imagem) {
     return conexaoConvertida;
 }
 
+// async function buscaVideo(termoDeBusca) {
+//     const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`);
+//     const conexaoConvertida = await conexao.json();
+
+//     return conexaoConvertida;
+// }
+
 async function buscaVideo(termoDeBusca) {
     const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`);
-    const conexaoConvertida = conexao.json();
+    const conexaoConvertida = await conexao.json();
 
-    return conexaoConvertida;
+    // Filtra os vídeos que têm o termo no título
+    const filtrados = conexaoConvertida.filter(video => 
+        video.titulo.toLowerCase().includes(termoDeBusca.toLowerCase())
+    );
+
+    return filtrados;
 }
 
 export const conectaApi = {

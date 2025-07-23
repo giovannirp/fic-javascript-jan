2c011069553b4d69d4dfeb41995edbebb4de74ef
giovanni <giovanni_ge@hotmai.com>
2025-07-22 22:51:53 -0300

    feat: adicionando componente do Link

--- luz-e-cena/src/App.tsx
+++ luz-e-cena/src/App.tsx
@@ -1,10 +1,12 @@
 import Banner from "./Components/Banner"
+import Link from "./Components/Link"
 
 function App() {
 
   return (
     <>
       <Banner src="./banner.png" alt="Banner"/>
+      <Link href="/" target="_blanck">Link</Link>
     </>
   )
 }

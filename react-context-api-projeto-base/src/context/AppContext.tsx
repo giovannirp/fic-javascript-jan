efcf8223f2079009ef84c24c4d7054e0dfe70052
Giovanni <giovanni_ge@hotmail.com>
2025-08-19 10:31:08 -0300

    feat: adicionando novos arquivos

--- react-context-api-projeto-base/src/context/AppContext.tsx
+++ react-context-api-projeto-base/src/context/AppContext.tsx
@@ -1,22 +1,28 @@
+/* eslint-disable react-refresh/only-export-components */
 import React, { createContext, useContext, useEffect, useState } from 'react';
-import { IUsuario } from '../types';
-import { criarUsuario, obterUsuario } from '../api';
+import { ITransacoes, IUsuario } from '../types';
+import { criarTransacao, criarUsuario, obterTransacoes, obterUsuario } from '../api';
 
 interface AppContextType {
     usuario: IUsuario | null;
     criaUsuario: (usuario: Omit<IUsuario, "id">) => Promise<void>;
+    transacoes: ITransacoes[];
+    criaTransacao: (novaTransacao: Omit<ITransacoes, "id">) => Promise<void>;
 }
 
 const AppContext = createContext<AppContextType | undefined>(undefined)
 
 const AppProvider = ({children}: {children: React.ReactNode}) => {
   const [usuario, setUsuario] = useState<IUsuario | null>(null);
+  const [transacoes, setTransacoes] = useState<ITransacoes[]>([]);
 
   const carregaDadosUsuarios = async () => {
     try {
         const usuarios = await obterUsuario();
+        const transacoes = await obterTransacoes();
         if (usuarios.length > 0) {
             setUsuario(usuarios[0]);
+            setTransacoes(transacoes);
         }
     } catch(err) {
         console.log(err);
@@ -34,10 +40,19 @@ const AppProvider = ({children}: {children: React.ReactNode}) => {
     } catch(err) {
         console.log(err);
     }
+  };
+
+  const criaTransacao = async (novaTransacao: Omit<ITransacoes, "id">) => {
+    try {
+      const transacoesCriada = await criarTransacao(novaTransacao);
+      setTransacoes((prev) => [...prev, transacoesCriada]);
+    } catch(err) {
+      console.error(err);
+    }
   }
 
   return (
-    <AppContext.Provider value={{ usuario, criaUsuario }}>
+    <AppContext.Provider value={{ usuario, criaUsuario, transacoes, criaTransacao }}>
         {children}
     </AppContext.Provider>
   )
